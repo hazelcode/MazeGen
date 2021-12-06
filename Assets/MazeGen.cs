c7d627563dac2a1fa9bf45adef96a4014578eb93
Ben Brown <hasben2345@gmail.com>
2021-12-06 16:09:27 -0500

    whatever

--- Assets/MazeGen.cs
+++ Assets/MazeGen.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -10,11 +11,11 @@ public class MazeGen : MonoBehaviour
     {
         int mW = 6;
         int mH = 6;
-        int[] groupID = Enumerable.Range(0, mW*mH).ToArray();
+        int[] groupID = Enumerable.Range(0, mW * mH).ToArray();
 
         //Initialize maze representation with 2D Array
-        int[,] mazeRep = new int[mW*mH, 4];
-        for (int i=0; i<mW*mH; i++)
+        int[,] mazeRep = new int[mW * mH, 4];
+        for (int i = 0; i < mW * mH; i++)
         {
             mazeRep[i, 0] = 0; //top
             mazeRep[i, 1] = 0; //left
@@ -30,74 +31,94 @@ public class MazeGen : MonoBehaviour
         System.Random r = new System.Random();
         edgeList = edgeList.OrderBy(x => r.Next()).ToArray();
 
-        int horizEdgeCount = (mH - 1) * mW;
-        int vertEdgeCount = (mW - 1) * mH;
-        int colNumb = 0;
-
-        int[] colNumbs = new int[vertEdgeCount];
-        for (int i = 0; i<vertEdgeCount; i++)
-        {
-            print(i + " column number = " + colNumb);
-            colNumbs[i] = colNumb;
-            colNumb++;
-            if (colNumb >= mW) colNumb = 0;
-        }
-        for (int i = 0; i<edgeCount; i++)
+        for (int i = 0; i < edgeCount; i++)
         {
             int edgeID = edgeList[i];
-            if (edgeID < horizEdgeCount - 1)
+            if (edgeID < (mW - 1) * mH)
             {
-                
+
                 int rowNumb = edgeID / (mW - 1);
-                print(edgeID + " row number = " + rowNumb);
-                
+
                 int V1 = edgeID + rowNumb;
                 int V2 = V1 + 1;
                 if (groupID[V1] != groupID[V2]) {
                     mazeRep[V1, 3] = 1; //right
                     mazeRep[V2, 1] = 1; //left
-                }
-                int oldG = groupID[V1];
-                int newG = groupID[V2];
-                for (int j = 0; j<mW*mH; j++)
-                {
-                    if (groupID[j] == oldG)
+                    int oldG = groupID[V1];
+                    int newG = groupID[V2];
+                    for (int j = 0; j < mW * mH; j++)
                     {
-                        groupID[j] = newG;
+                        if (groupID[j] == oldG)
+                        {
+                            groupID[j] = newG;
+                        }
                     }
                 }
-                
             }
             else
             {
-                print(edgeID + " - " + vertEdgeCount + " = " + (edgeID - vertEdgeCount));
-                print(edgeID + " column number = " + colNumbs[edgeID-vertEdgeCount]);
-                print("that means that the upper vert is:" + (edgeID-vertEdgeCount));
-                print("and the lower vert is: " + (edgeID - vertEdgeCount + mW));
-                
-                int V1 = (edgeID - vertEdgeCount);
-                int V2 = (edgeID - vertEdgeCount + mW);
+                edgeID -= (mW - 1) * mH;
+                int V1 = edgeID;
+                int V2 = V1 + mW;
                 if (groupID[V1] != groupID[V2])
                 {
                     mazeRep[V1, 2] = 1; //top
                     mazeRep[V2, 0] = 1; //bottom
-                }
-                int oldG = groupID[V1];
-                int newG = groupID[V2];
-                for (int j = 0; j < mW * mH; j++)
-                {
-                    if (groupID[j] == oldG)
+                    int oldG = groupID[V1];
+                    int newG = groupID[V2];
+                    for (int j = 0; j < mW * mH; j++)
                     {
-                        groupID[j] = newG;
+                        if (groupID[j] == oldG)
+                        {
+                            groupID[j] = newG;
+                        }
                     }
                 }
-                
-
             }
         }
+        int xx = 0;
+        int yy = 0;
+        /*
+        //Initialize tiles with 2D Array
+        int[,] tileRep = new int[(mW - 1) * (mH - 1), 4];
+        for (int i = 0; i < (mW-1) * (mH-1); i++)
+        {
+            mazeRep[i, 0] = 0; //top free
+            mazeRep[i, 1] = 0; //left free
+            mazeRep[i, 2] = 0; //bottom free
+            mazeRep[i, 3] = 0; //right free
+        }
+        
+        int tilexx = 0;
+        int tileyy = 0;
+
+        for (int i = 0; i < (mW-1) * (mH-1); i++) {
+            print("tile " + i + ": x: " + tilexx + ", yy:" + tileyy);
+
+            tilexx++;
+            if (tilexx == mW-1)
+            {
+                tilexx = 0;
+                tileyy += 1;
+            }
+        }*/
+
         for (int i = 0; i < mW * mH; i++)
         {
-            print("vert " + i + ": " + mazeRep[i, 0] + ", " + mazeRep[i, 1] + ", " + mazeRep[i, 2] + ", " + mazeRep[i, 3]);
+            int T = mazeRep[i, 0];
+            int L = mazeRep[i, 1];
+            int B = mazeRep[i, 2];
+            int R = mazeRep[i, 3];
+            String representation = "" + T + L + B + R;
+            //print("x: " + xx + ", y: " + yy + ", current: " + representation);
+            GameObject tile = Resources.Load(representation, typeof(GameObject)) as GameObject;
+            Instantiate(tile, new Vector3(xx, 0, yy), Quaternion.identity);
+            xx += 1;
+            if (xx==mW)
+            {
+                xx = 0;
+                yy += 1;
+            }
         }
     }
 
